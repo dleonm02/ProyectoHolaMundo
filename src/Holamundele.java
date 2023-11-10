7fd5b8bfd971d9c02450bafcb7385332d6ba5b06
dleonm02 <dleonm02>
2023-11-10 13:06:08 +0100

    Bienvenida

---
+++ src/Holamundele.java
@@ -0,0 +1,9 @@
+
+public class Holamundele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Holapersona.java
@@ -0,0 +1,9 @@
+
+public class Holapersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
